39b26e8fe76529efda8412323bfa1a2263aea459
Marcus Souza <marcusricardo2703@gmail.com>
2025-01-29 23:39:07 -0300

    Atualização Aula 3
    - Atualização organização pastas e arquivos;
    - Refatoração CSS utilizando variáveis;
    - Edição conteúdo principal:
      * main-container;
      * header__navigation;
      * playlist-container;
    - Edição folhas de estilos: vars.css, sidebar-footer.css, main-content.css;
    - Edição arquivo JavaScript função **greetings** e ** exibirMensagemgreeting **.

--- script.js
+++ script.js
@@ -3,7 +3,33 @@ document.getElementById('botao-contraste').addEventListener('click', function ()
 
     const logo = document.getElementById('logo-spotify'); // Seleciona a imagem
     let nomeLogo = document.body.classList.contains('contraste') ? 'Spotify_Full_Logo_RGB_Black.png' : 'Spotify_Full_Logo_RGB_White.png'; // Altera imagem para modo contraste
-    console.log(nomeLogo);
-    logo.src = `assets/icons/${nomeLogo}`; // define o novo src da imagem
+    logo.src = `./src/assets/icons/${nomeLogo}`; // define o novo src da imagem
     
 });
+
+function greetings(){
+    let data = new Date();
+    let hora = data.getHours();
+    let saudacao = "Bom dia!";
+
+    if(hora >= 12 && hora < 18)
+    {
+        saudacao = "Boa tarde!";
+    }
+    else if(hora >= 18)
+    {
+        saudacao = "Boa noite!";
+    }
+
+    return saudacao;
+}
+
+function exibirTextoNaTela(id, texto){
+    let campo = document.getElementById (id);
+    campo.innerHTML = texto;
+}
+
+function exibirMensagemgreeting(){
+exibirTextoNaTela('greeting', greetings());
+}
+exibirMensagemgreeting();
